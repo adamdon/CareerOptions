fc8fc96a54e7d12c997677d53c8a256bcf36f10d
Adam Don <adamdon89@gmail.com>
2020-12-05 14:16:54 +0000

    javascript refactor

--- javascript/table_scripts.js
+++ javascript/table_scripts.js
@@ -1,64 +1,53 @@
-var $table = $('#table')
-
 $(function()
 {
-    var xmlhttp = new XMLHttpRequest();
-    var jsonObject = null;
-    var data2
-
-    data2 = [
-        {
-            'soc': 0,
-            'title': 'Item 0',
-        }
-    ];
-
-    xmlhttp.onreadystatechange = function()
-    {
-        if (this.readyState === 4 && this.status === 200)
-        {
-            jsonObject = JSON.parse(this.responseText);
-            data2 = jsonObject;
-            $table.bootstrapTable({data: data2})
-        }
-        else
-        {
-            //document.getElementById("surveyResultsBox").innerHTML ='<h3>Loading failed</h3>';
-        }
-    };
-    xmlhttp.open("GET", "http://api.lmiforall.org.uk/api/v1/soc/search?q=Software%20Developer", true);
-    xmlhttp.send();
+    var keywordText;
 
 
-
-
-
-    // var data = [
-    //     {
-    //         'soc': 0,
-    //         'title': 'Item 0',
-    //     },
-    //     {
-    //         'soc': 1,
-    //         'title': 'Item 1',
-    //     },
-    //     {
-    //         'soc': 2,
-    //         'title': 'Item 2',
-    //     },
-    //     {
-    //         'soc': 3,
-    //         'title': 'Item 3',
-    //     },
-    //     {
-    //         'soc': 4,
-    //         'title': 'Item 4',
-    //     },
-    //     {
-    //         'soc': 5,
-    //         'title': 'Item 5',
-    //     }
-    // ]
-
-
+    keywordText = "software";
+    performLmiforallApiSearch(keywordText);
 })
+
+
+
+
+function performLmiforallApiSearch(toSearchKeywordText)
+{
+    var xmlHttpRequest = new XMLHttpRequest();
+    var baseUrlText = "http://api.lmiforall.org.uk/api/v1/soc/search?q=";
+    var returnedJsonObject = null;
+
+
+    xmlHttpRequest.onreadystatechange = function()
+    {
+        if(this.readyState === 4 )
+        {
+            if(this.status === 200)
+            {
+                returnedJsonObject = JSON.parse(this.responseText);
+                updateTableWithData(returnedJsonObject);
+            }
+            else if (this.status === 0)
+            {
+                returnedJsonObject = [{ 'soc': this.status, 'title': 'Data API down',}, {'soc': 0, 'title': ' ',},];
+                updateTableWithData(returnedJsonObject);
+            }
+        }
+
+    };
+    xmlHttpRequest.open("GET", (baseUrlText + toSearchKeywordText), true);
+    xmlHttpRequest.send();
+}
+
+
+
+
+
+
+
+function updateTableWithData(toUpdateWithData)
+{
+    var tableElement;
+
+    tableElement = $('#table');
+    tableElement.bootstrapTable({data: toUpdateWithData});
+}
